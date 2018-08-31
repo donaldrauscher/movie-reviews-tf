6534643d5ae926eb837c788128b5099268c9bbd9
Donald Rauscher <donald.rauscher@gmail.com>
2018-08-31 18:27:49 -0500

    Add custom estimator

--- serve_deploy.sh
+++ serve_deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 MODEL_NAME=movie_reviews
-MODEL_VERSION=v1
+MODEL_VERSION=v2
 MODEL_TIMESTAMP=$(ls -t exports/ | head -1)
 
 DEPLOYMENT_SOURCE=gs://djr-data/movie-reviews
 
 gsutil rsync -c -d -r exports/$MODEL_TIMESTAMP $DEPLOYMENT_SOURCE
 
-gcloud ml-engine models create $MODEL_NAME
+#gcloud ml-engine models create $MODEL_NAME
 
 gcloud ml-engine versions create $MODEL_VERSION --model $MODEL_NAME --origin $DEPLOYMENT_SOURCE \
     --python-version 3.5 --runtime-version 1.9
