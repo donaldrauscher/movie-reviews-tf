c2659477594033e0cc54895d482820c58c7b897f
Donald Rauscher <donald.rauscher@gmail.com>
2018-08-31 18:49:37 -0500

    JSON-->NDJSON

--- serve_test.sh
+++ serve_test.sh
@@ -3,4 +3,4 @@
 MODEL_NAME=movie_reviews
 MODEL_VERSION=v1
 
-gcloud ml-engine predict --model movie_reviews --version v2 --json-instances reviews.json
+gcloud ml-engine predict --model movie_reviews --version v2 --json-instances reviews.ndjson
